a2a6025256a069447c4ea131b55336da3feb60a7
Ryan Gibbons <rtgibbons23@gmail.com>
2025-06-10 16:01:56 -0500

    Sync files from upstream with rsync removing files from source
        rsync -avh --delete --exclude .git upstream-ceph/* proxmox-ceph/ceph/
    
    Restore files of gitmodues that were statically added to proxmox repo.
    This means I didn't verify the commit versions of Proxmox to upstream.
    
       for foo in `awk '/path/ {print $3}' .gitmodules`; do git co $foo; done

--- ceph/src/pmdk/src/tools/pmempool/transform.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
-/* Copyright 2016-2020, Intel Corporation */
-
-/*
- * transform.h -- pmempool transform command header file
- */
-
-int pmempool_transform_func(const char *appname, int argc, char *argv[]);
-void pmempool_transform_help(const char *appname);
--- ceph/src/pybind/mgr/rook/rook_client/__init__.py
+++
@@ -1,1 +0,0 @@
-from ._helper import STRICT
